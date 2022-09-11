f269d992df47ce7f990b2216d76986bcbd0c16ac
Jonah Warren <jonahewarren@gmail.com>
2022-09-11 16:37:18 -0500

    Updated to include frames and color

--- main.py
+++ main.py
@@ -2,5 +2,21 @@ from tkinter import *
 
 
 root = Tk()
+# Override the settings of the window
+root.configure(bg="black")
+root.geometry('1200x600')
+root.title("Minesweeper by Jonah :)")
+root.resizable(False, False)
 
+top_frame = Frame(
+    root,
+    bg='blue', # Change later to turquoise
+    width=1200,
+    height=150
+)
+top_frame.place(x=0, y=0)
+
+
+
+# run the window
 root.mainloop()
